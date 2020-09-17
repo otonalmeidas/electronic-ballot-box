39b973d9e987a3478e8e7cb6121c78a96209f06e
Wellington Almeida <otonalmeidas@hotmail.com>
2020-09-16 23:48:25 -0300

    :up: JS com as fases

---
+++ phases.js
@@ -0,0 +1,50 @@
+let phases = [
+    {
+        title: 'VEREADOR',
+        numbers: 5,
+        candidates: [
+            {
+                number: 12345,
+                name: 'Fulana de Tal',
+                broken: 'ABC',
+                photo: [
+                    {url:'12345.jpg', legend: 'Vereador', small: false}
+                ]
+            },
+            {
+                number: 54321,
+                name: 'Beltrano da Silva',
+                partido: 'CBA',
+                photo: [
+                    {url:'54321.jpg', legend: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        title: 'PREFEITO',
+        numbers: 2,
+        candidates: [
+            {
+                number: 12,
+                name: 'Zulano',
+                broken: 'ABC',
+                vice: 'Zul',
+                photo: [
+                    {url:'12.jpg', legend: 'Prefeito'},
+                    {url:'123.jpg', legend: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                number: 21,
+                name: 'Ciclana',
+                broken: 'CBA',
+                Vice: 'Cic',
+                photo:[
+                    {url:'21.jpg', legend: 'Prefeita', small: true},
+                    {url:'210.jpg', legend: 'Vice-Prefeita', small: true}
+                ]
+            }
+        ]
+    },
+];
