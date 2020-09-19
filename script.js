563d4a7ccb140f62bf4b3ef5388f484721dd9813
Wellington Almeida <otonalmeidas@hotmail.com>
2020-09-18 23:27:29 -0300

    Correções

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ let aviso = document.querySelector('.second-division');
 let lateral = document.querySelector('.right');
 let numeros = document.querySelector('.digits');
 
-let eltapaAtual = 0;
-
-function comecarEtapa() {
-    let etapa = phases[etapaAtual];
-
-    let numeroHTML = '';
-
-    seuVotoPara.style.display = 'none';
-    cargo.innerHTML = etapa.title;
-    descricao.innerHTML = '';
-    aviso.style.display = 'none';
-    lateral.innerHTML = '';
-    numeros.innerHTML = numeroHTML;
-}
-
 function clicou(n) {
     alert("Clicou em "+n);
 }
@@ -32,5 +17,3 @@ function corrige() {
 function confirma() {
     alert("Clicou em CONFIRMA!");
 }
-
-comecarEtapa();
